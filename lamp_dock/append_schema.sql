1da5ea61cb0bad407ab0499ec0d1e0f3518835cc
Chisato Sakamoto <ex.t.and.jsb3.g@gmail.com>
2020-11-23 18:53:10 +0900

    CURRENT_TIMESTAMPについてSQL修正#10

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE purchased_history (
     purchased_id INT(11) AUTO_INCREMENT,
     user_id INT(11),
-    purchased_datetime DATETIME CURRENT_TIMESTAMP,
+    purchased_datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(purchased_id)
 );
 
@@ -11,7 +11,7 @@ CREATE TABLE purchased_history_detail (
     purchased_name VARCHAR(100) COLLATE utf8_general_ci,
     purchased_price INT(11),
     purchased_amount INT(11),
-    created DATETIME CURRENT_TIMESTAMP,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     FOREIGN KEY(purchased_id)
     REFERENCES purchased_history(purchased_id)
